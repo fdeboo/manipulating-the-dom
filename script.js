10ea346ec946b38e5df6afea6a6399bca8b3b9f5
fran2488 <frances.deboo@gmail.com>
2020-01-21 11:43:44 +0000

    jquery events lesson complete

--- script.js
+++ script.js
@@ -17,4 +17,17 @@ $(document).ready(function() {
         $(".stream3").removeClass('highlight_stream');
         $(".stream3").addClass('highlight_stream');
    });
+   $("p").click(function() {
+        $("p").css('color', 'red');
+   });
+   $("h2").mouseenter(function() {
+        $("h2").css('background-color', 'lightblue');
+        $("#card1, #card2, #card3, #card4, #card5, #card6").css('font-size', '1.5em');
+   });
+   $("a").mouseenter(function() {
+       $("body").css("background-color", "#000");
+   });
+   $("a").mouseleave(function() {
+       $("body").css("background-color", "#eee");
+   });
 }); 
