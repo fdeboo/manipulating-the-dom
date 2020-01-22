69a8d8f692f1071aed8ef7b806e13c53a60dc0fb
fran2488 <frances.deboo@gmail.com>
2020-01-22 17:04:29 +0000

    challenges complete

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
-    fgh
+    //$(".theButton").click(function() {
+       // $(".theButton").hide();
+    //});
+
+    //$(".theButton").click(function() {
+        //$(this).hide();
+    //});
+
+    $( ".theButton" ).click(function() {
+        $("#panel .container").siblings().fadeTo( "slow", 0.1 );
+    });
+
+    $( ".superButton" ).click(function() {
+        $("#panel .container").siblings().fadeTo( "slow", 1.0 );
+    });
+
+    $( ".theButton" ).mouseenter(function() {
+        $(this).addClass("makeBlack");
+    });
+
+    $( ".theButton" ).mouseleave(function() {
+        $(this).removeClass( "makeBlack" );
+    });
 });
